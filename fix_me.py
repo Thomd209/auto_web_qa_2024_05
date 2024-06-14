8e2d524548fbcdaedca5f5d4206745043af248a4
d.tomas <d.tomas@gardatech.ru>
2024-06-14 12:06:09 +0300

    Add linter. Fix mistakes

--- fix_me.py
+++ fix_me.py
@@ -1,2 +1,9 @@
-print("Hello world!")
-print("Hello world!")
+def calculate_average(nums):
+    total = sum(nums)
+    count = len(nums)
+    return total / count
+
+
+nums = [10, 15, 20]
+result = calculate_average(nums)
+print("The average is:", result)
